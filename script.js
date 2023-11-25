f13af8a29f6be7c35206a843b859acd198574677
vishwajagtap <57266690+vishwajagtap@users.noreply.github.com>
2023-11-25 19:13:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function openBook() {
+  const bookCover = document.getElementById('bookCover');
   const book = document.getElementById('book');
+  
+  // Show the book cover image
+  bookCover.classList.remove('hidden');
+  
+  // Hide the button
+  event.target.style.display = 'none';
+  
+  // Show the book with your message
   book.classList.remove('hidden');
 }
+
