148db0331542facac746ccb86ff962c6a0ba9e19
vishwajagtap <57266690+vishwajagtap@users.noreply.github.com>
2023-11-25 19:56:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 function openBook() {
   const book = document.getElementById('book');
-  const bookImage = document.querySelector('.book-image');
-
-  document.querySelector('.button').style.display = 'none';
-  book.style.backgroundImage = 'url("book.jpg")';
-  bookImage.classList.remove('hidden');
-  document.querySelector('.button.hidden').classList.remove('hidden');
+  const openBtn = document.querySelector('.open-btn');
+  const boy = document.getElementById('boy');
+  const speakBtn = document.querySelector('.speak-btn');
+  
+  // Hide the 'Open the Book' button and reveal the book image
+  openBtn.style.display = 'none';
+  book.classList.remove('hidden');
+  
+  // Show the boy image
+  boy.classList.remove('hidden');
+  speakBtn.classList.remove('hidden');
 }
 
-function showBoyAndText() {
-  document.getElementById('boy').classList.remove('hidden');
-  document.getElementById('message').classList.remove('hidden');
+function openMessage() {
+  const message = document.getElementById('message');
+  const speakBtn = document.querySelector('.speak-btn');
+  
+  // Hide the 'I Want to Say Something' button and reveal the message
+  speakBtn.style.display = 'none';
+  message.classList.remove('hidden');
 }
