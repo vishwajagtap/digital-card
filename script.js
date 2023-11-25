643d51d171b2cf98c1c0a967c76203e14532ae33
vishwajagtap <57266690+vishwajagtap@users.noreply.github.com>
2023-11-25 19:31:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 function openBook() {
-  const bookCover = document.getElementById('bookCover');
-  const book = document.getElementById('book');
-  
-  // Show the book cover image
-  bookCover.classList.remove('hidden');
-  
-  // Hide the button
-  event.target.style.display = 'none';
-  
-  // Show the book with your message
-  book.classList.remove('hidden');
+  const bookImage = document.getElementById('bookImage');
+  const bookContent = document.getElementById('bookContent');
+  const openButton = document.querySelector('.open-button');
+
+  // Hide the book image and open button
+  bookImage.style.display = 'none';
+  openButton.style.display = 'none';
+
+  // Show the book content
+  bookContent.classList.remove('hidden');
 }
 
+function showMessage() {
+  const boyImage = document.querySelector('.boy-image');
+  const speakButton = document.querySelector('.speak-button');
+  const messageContent = document.getElementById('messageContent');
+
+  // Show the boy image and speak button
+  boyImage.style.display = 'block';
+  speakButton.style.display = 'block';
+
+  // Show the message content
+  messageContent.classList.remove('hidden');
+}
