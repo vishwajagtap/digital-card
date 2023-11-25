e48e10fe523aa1692f16d522810bbdbfb0824661
vishwajagtap <57266690+vishwajagtap@users.noreply.github.com>
2023-11-25 19:48:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function openBook() {
   const bookImage = document.querySelector('.book-image');
 
   document.querySelector('.button').style.display = 'none';
-  book.style.backgroundImage = 'url("book_image.jpg")';
+  book.style.backgroundImage = 'url("book.jpg")';
   bookImage.classList.remove('hidden');
   document.querySelector('.button.hidden').classList.remove('hidden');
 }
